881499e3574ade5f3c6dfa105e593d61d879d734
Jeongmin Kim <minkim888@naver.com>
2021-05-30 17:30:53 +0900

    parallelization

---
+++ R/__imports.R
@@ -0,0 +1,31 @@
+#' @import lightgbm
+#' @import Matrix
+#' @import tictoc
+#' @import ggrepel
+#' @import bigstatsr
+#' @import data.table
+#' @import tidyverse
+#' @import tibble
+#' @import lubridate
+#' @import xts
+#' @import PerformanceAnalytics
+#' @import magrittr
+#' @import quantmod
+#' @import RiskPortfolios
+#' @import rBayesianOptimization
+#' @import polyclip
+#' @import tweenr
+#' @import rstatix
+#' @import ROCR
+#' @import pROC
+#' @import Metrics
+#' @import sna
+#' @import remotes
+#' @import zoo
+#' @import purrr
+#' @import stringr
+#' @import reshape2
+#' @importFrom foreach 'foreach' `%dopar%`
+#' @importFrom parallel 'makeCluster' 'stopCluster'
+#' @importFrom doParallel 'registerDoParallel'
+NULL
