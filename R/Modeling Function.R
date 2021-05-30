80b74ebb7c5af6f9fb33a0266592ffa1afa860c6
Jeongmin Kim <minkim888@naver.com>
2021-05-30 20:35:19 +0900

    shap for lgbm in modeling function

--- R/Modeling Function.R
+++ R/Modeling Function.R
@@ -2,7 +2,7 @@
 # Return and Save Selected Stock List (SSL)
 
 #' @export
-modeling_func = function(df, target_y, title = "", train_span=36, push_span=1, ensemble_n = 300, bagging_prop=0.8, feature_prop=0.8, num_rounds=60, pred_start_date = '2017-01-01', explain_yn = 'N') {
+modeling_func = function(df, target_y, title = "", train_span=36, push_span=1, ensemble_n = 300, bagging_prop=0.8, feature_prop=0.8, num_rounds=60, pred_start_date = '2017-01-01', explain_yn = 'Y') {
 
   pred_list <- list()
   rmse_valid_vec = c()
@@ -92,29 +92,32 @@ modeling_func = function(df, target_y, title = "", train_span=36, push_span=1, e
       select(date, stock_cd, contains("pred")) %>%
       ungroup()
 
+    if(explain_yn == 'Y'){
 
-    # SHAP
-    shap_train <- shap.prep(xgb_model = lgbm_model, X_train = as.matrix(dt_train %>% sample_n(1000) %>% select(-c(`stock_cd`,`date`, contains('target'), contains('return')))))
-    shap_test <- shap.prep(xgb_model = lgbm_model, X_train = as.matrix(dt_test %>% select(-c(`stock_cd`,`date`, contains('target'), contains('return')))))
+      # SHAP
+      shap_train <- shap.prep(xgb_model = lgbm_model, X_train = data.matrix(dt_train %>% sample_n(1000) %>% select(-c(`stock_cd`,`date`, contains('target'), contains('return')))))
+      shap_test <- shap.prep(xgb_model = lgbm_model, X_train = data.matrix(dt_test %>% select(-c(`stock_cd`,`date`, contains('target'), contains('return')))))
 
-    shap_train_df <-
-      rbind(
-        shap_train_df,
-        shap_train %>%
-          mutate(date = unique(dt_test$date)) %>%
-          select(date, variable, value, rfvalue, stdfvalue, mean_value)
-      )
+      shap_train_df <-
+        rbind(
+          shap_train_df,
+          shap_train %>%
+            mutate(date = unique(dt_test$date)) %>%
+            select(date, variable, value, rfvalue, stdfvalue, mean_value)
+        )
 
-    shap_test_df <-
-      rbind(
-        shap_test_df,
-        shap_test %>%
-          left_join(
-            cbind(data.frame(ID=sort(unique(shap_test$ID))), dt_test %>% select(date, stock_cd)),
-            by="ID"
-          ) %>%
-          select(date, stock_cd, variable, value, rfvalue, stdfvalue, mean_value)
-      )
+      shap_test_df <-
+        rbind(
+          shap_test_df,
+          shap_test %>%
+            left_join(
+              cbind(data.frame(ID=sort(unique(shap_test$ID))), dt_test %>% select(date, stock_cd)),
+              by="ID"
+            ) %>%
+            select(date, stock_cd, variable, value, rfvalue, stdfvalue, mean_value)
+        )
+
+    }
 
     # saving on 'selected_stock_cd_list'
     selected_stock_cd_list[[k]] <- top_list
