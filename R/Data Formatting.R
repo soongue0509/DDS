fcc3f9fc56dabe8fc5476ec48b2c65b00c08376f
Sang Hyun <40751693+sanghyun614@users.noreply.github.com>
2021-06-06 15:08:10 +0900

    Update Data Formatting.R
    deleted unnecessary arguments

--- R/Data Formatting.R
+++ R/Data Formatting.R
@@ -2,14 +2,13 @@
 # Prepare Data for Modeling
 
 #' @export
-df_prep_model = function(df, var1 = 'target_1m_updown_index', var2 = 'target_1m_updown_10', beg_date = '2011-01-01'){
+df_prep_model = function(df, beg_date = '2011-01-01'){
 
   formatted = df %>%
     mutate(date = ymd(date)) %>%
     mutate(stock_cd = str_pad(stock_cd, 6,side = c('left'), pad = '0')) %>%
-    filter(date >= as.Date(beg_date)) %>%
-    filter(!is.na(get(var1))) %>%
-    filter(!is.na(get(var2))) %>% as.data.frame()
+    filter(date >= as.Date(beg_date)) %>% 
+    as.data.frame()
 
   return(formatted)
 
