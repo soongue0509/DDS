62aa577dc0e5b67e65fe8a78a9b710d1455001a8
Sang Hyun <40751693+sanghyun614@users.noreply.github.com>
2021-06-06 16:31:19 +0900

    Update Data Formatting.R
    앞에 as.data.frame 넣어줬습니다. 안해주면 직접 해야되는데 귀찮자나여 ㅋㅋ

--- R/Data Formatting.R
+++ R/Data Formatting.R
@@ -5,6 +5,7 @@
 df_prep_model = function(df, beg_date = '2011-01-01'){
 
   formatted = df %>%
+    as.data.frame() %>%
     mutate(date = ymd(date)) %>%
     mutate(stock_cd = str_pad(stock_cd, 6,side = c('left'), pad = '0')) %>%
     filter(date >= as.Date(beg_date)) %>% 
