113fbff9702bd529431ff9b640ab1f4530ac21ff
Sang Hyun <40751693+sanghyun614@users.noreply.github.com>
2021-09-26 17:47:44 +0900

    Update Interpret.R

--- R/Interpret.R
+++ R/Interpret.R
@@ -257,4 +257,389 @@ eda_shap_newvars = function(shap_new, shap_old, features, years = '1y', scope = 
 
 }
 
-
+#' @export
+shap_viz <- function(test_shap) {
+  
+  StatSina <- ggproto('StatSina', Stat, required_aes = c('x', 'y'),
+                      setup_data = function(data, params) {
+                        if (is.double(data$x) && !.has_groups(data) && any(data$x != data$x[1L])) {
+                          stop('Continuous x aesthetic -- did you forget aes(group=...)?',
+                               call. = FALSE
+                          )
+                        }
+                        
+                        data
+                      },
+                      
+                      setup_params = function(data, params) {
+                        params$maxwidth <- params$maxwidth %||% (resolution(data$x %||% 0) * 0.9)
+                        
+                        if (is.null(params$binwidth) && is.null(params$bins)) {
+                          params$bins <- 50
+                        }
+                        
+                        params
+                      },
+                      
+                      compute_panel = function(self, data, scales, scale = TRUE, method = 'density',
+                                               bw = 'nrd0', kernel = 'gaussian', binwidth = NULL,
+                                               bins = NULL, maxwidth = 1, adjust = 1, bin_limit = 1,
+                                               seed = NA) {
+                        
+                        if (!is.null(binwidth)) {
+                          bins <- bin_breaks_width(scales$y$dimension() + 1e-8, binwidth)
+                        } else {
+                          bins <- bin_breaks_bins(scales$y$dimension() + 1e-8, bins)
+                        }
+                        
+                        data <- ggproto_parent(Stat, self)$compute_panel(data, scales,
+                                                                         scale = scale, method = method, bw = bw, kernel = kernel,
+                                                                         bins = bins$breaks, maxwidth = maxwidth, adjust = adjust,
+                                                                         bin_limit = bin_limit)
+                        
+                        if (is.logical(scale)) {
+                          scale <- if (scale) 'area' else 'width'
+                        }
+                        # choose how sinas are scaled relative to each other
+                        data$sinawidth <- switch(
+                          scale,
+                          # area : keep the original densities but scale them to a max width of 1
+                          #        for plotting purposes only
+                          area = data$density / max(data$density),
+                          # count: use the original densities scaled to a maximum of 1 (as above)
+                          #        and then scale them according to the number of observations
+                          count = data$density / max(data$density) * data$n / max(data$n),
+                          # width: constant width (each density scaled to a maximum of 1)
+                          width = data$scaled
+                        )
+                        
+                        if (!is.na(seed)) {
+                          new_seed <- sample(.Machine$integer.max, 1L)
+                          set.seed(seed)
+                          on.exit(set.seed(new_seed))
+                        }
+                        data$xmin <- data$x - maxwidth / 2
+                        data$xmax <- data$x + maxwidth / 2
+                        data$x_diff <- runif(nrow(data), min = -1, max = 1) *
+                          maxwidth * data$sinawidth/2
+                        data$width <- maxwidth
+                        
+                        # jitter y values if the input is input is integer
+                        if (all(data$y == floor(data$y))) {
+                          data$y <- jitter(data$y)
+                        }
+                        
+                        data
+                      },
+                      
+                      compute_group = function(data, scales, scale = TRUE, method = 'density',
+                                               bw = 'nrd0', kernel = 'gaussian',
+                                               maxwidth = 1, adjust = 1, bin_limit = 1,
+                                               bins = NULL) {
+                        if (nrow(data) == 0) return(NULL)
+                        
+                        if (nrow(data) < 3) {
+                          data$density <- 0
+                          data$scaled <- 1
+                        } else if (method == 'density') { # density kernel estimation
+                          range <- range(data$y, na.rm = TRUE)
+                          bw <- calc_bw(data$y, bw)
+                          dens <- compute_density(data$y, data$w, from = range[1], to = range[2],
+                                                  bw = bw, adjust = adjust, kernel = kernel)
+                          densf <- stats::approxfun(dens$x, dens$density, rule = 2)
+                          
+                          data$density <- densf(data$y)
+                          data$scaled <- data$density / max(dens$density)
+                          
+                          data
+                        } else { # bin based estimation
+                          bin_index <- cut(data$y, bins, include.lowest = TRUE, labels = FALSE)
+                          data$density <- tapply(bin_index, bin_index, length)[as.character(bin_index)]
+                          data$density[data$density <= bin_limit] <- 0
+                          data$scaled <- data$density / max(data$density)
+                        }
+                        
+                        # Compute width if x has multiple values
+                        if (length(unique(data$x)) > 1) {
+                          width <- diff(range(data$x)) * maxwidth
+                        } else {
+                          width <- maxwidth
+                        }
+                        data$width <- width
+                        data$n <- nrow(data)
+                        data$x <- mean(range(data$x))
+                        data
+                      },
+                      finish_layer = function(data, params) {
+                        # rescale x in case positions have been adjusted
+                        x_mod <- (data$xmax - data$xmin) / data$width
+                        data$x <- data$x + data$x_diff * x_mod
+                        data
+                      },
+                      extra_params = 'na.rm'
+  )
+  
+  stat_sina <- function(mapping = NULL, data = NULL, geom = 'sina',
+                        position = 'dodge', scale = 'area', method = 'density',
+                        bw = 'nrd0', kernel = 'gaussian', maxwidth = NULL,
+                        adjust = 1, bin_limit = 1, binwidth = NULL, bins = NULL,
+                        seed = NA, ..., na.rm = FALSE, show.legend = NA,
+                        inherit.aes = TRUE) {
+    method <- match.arg(method, c('density', 'counts'))
+    
+    layer(
+      data = data,
+      mapping = mapping,
+      stat = StatSina,
+      geom = geom,
+      position = position,
+      show.legend = show.legend,
+      inherit.aes = inherit.aes,
+      params = list(scale = scale, method = method, bw = bw, kernel = kernel,
+                    maxwidth = maxwidth, adjust = adjust, bin_limit = bin_limit,
+                    binwidth = binwidth, bins = bins, seed = seed, na.rm = na.rm, ...)
+    )
+  }
+  
+  geom_sina <- function(mapping = NULL, data = NULL,
+                        stat = 'sina', position = 'dodge',
+                        ...,
+                        na.rm = FALSE,
+                        show.legend = NA,
+                        inherit.aes = TRUE) {
+    layer(
+      data = data,
+      mapping = mapping,
+      stat = stat,
+      geom = GeomPoint,
+      position = position,
+      show.legend = show.legend,
+      inherit.aes = inherit.aes,
+      params = list(
+        na.rm = na.rm,
+        ...
+      )
+    )
+  }
+  
+  bins <- function(breaks, closed = c('right', 'left'),
+                   fuzz = 1e-08 * stats::median(diff(breaks))) {
+    stopifnot(is.numeric(breaks))
+    closed <- match.arg(closed)
+    
+    breaks <- sort(breaks)
+    # Adapted base::hist - this protects from floating point rounding errors
+    if (closed == 'right') {
+      fuzzes <- c(-fuzz, rep.int(fuzz, length(breaks) - 1))
+    } else {
+      fuzzes <- c(rep.int(-fuzz, length(breaks) - 1), fuzz)
+    }
+    
+    structure(
+      list(
+        breaks = breaks,
+        fuzzy = breaks + fuzzes,
+        right_closed = closed == 'right'
+      ),
+      class = 'ggplot2_bins'
+    )
+  }
+  
+  compute_density <- function(x, w, from, to, bw = "nrd0", adjust = 1,
+                              kernel = "gaussian", n = 512) {
+    nx <- length(x)
+    if (is.null(w)) {
+      w <- rep(1 / nx, nx)
+    }
+    
+    # if less than 2 points return data frame of NAs and a warning
+    if (nx < 2) {
+      warning("Groups with fewer than two data points have been dropped.", call. = FALSE)
+      return(new_data_frame(list(
+        x = NA_real_,
+        density = NA_real_,
+        scaled = NA_real_,
+        ndensity = NA_real_,
+        count = NA_real_,
+        n = NA_integer_
+      ), n = 1))
+    }
+    
+    dens <- stats::density(x, weights = w, bw = bw, adjust = adjust,
+                           kernel = kernel, n = n, from = from, to = to)
+    
+    new_data_frame(list(
+      x = dens$x,
+      density = dens$y,
+      scaled =  dens$y / max(dens$y, na.rm = TRUE),
+      ndensity = dens$y / max(dens$y, na.rm = TRUE),
+      count =   dens$y * nx,
+      n = nx
+    ), n = length(dens$x))
+  }
+  calc_bw <- function(x, bw) {
+    if (is.character(bw)) {
+      if (length(x) < 2)
+        stop("need at least 2 points to select a bandwidth automatically", call. = FALSE)
+      bw <- switch(
+        base::tolower(bw),
+        nrd0 = stats::bw.nrd0(x),
+        nrd = stats::bw.nrd(x),
+        ucv = stats::bw.ucv(x),
+        bcv = stats::bw.bcv(x),
+        sj = ,
+        `sj-ste` = stats::bw.SJ(x, method = "ste"),
+        `sj-dpi` = stats::bw.SJ(x, method = "dpi"),
+        stop("unknown bandwidth rule")
+      )
+    }
+    bw
+  }
+  
+  bin_breaks <- function(breaks, closed = c('right', 'left')) {
+    bins(breaks, closed)
+  }
+  
+  bin_breaks_width <- function(x_range, width = NULL, center = NULL,
+                               boundary = NULL, closed = c('right', 'left')) {
+    stopifnot(length(x_range) == 2)
+    
+    # if (length(x_range) == 0) {
+    #   return(bin_params(numeric()))
+    # }
+    stopifnot(is.numeric(width), length(width) == 1)
+    if (width <= 0) {
+      stop('`binwidth` must be positive', call. = FALSE)
+    }
+    
+    if (!is.null(boundary) && !is.null(center)) {
+      stop('Only one of \'boundary\' and \'center\' may be specified.')
+    } else if (is.null(boundary)) {
+      if (is.null(center)) {
+        # If neither edge nor center given, compute both using tile layer's
+        # algorithm. This puts min and max of data in outer half of their bins.
+        boundary <- width / 2
+      } else {
+        # If center given but not boundary, compute boundary.
+        boundary <- center - width / 2
+      }
+    }
+    
+    # Find the left side of left-most bin: inputs could be Dates or POSIXct, so
+    # coerce to numeric first.
+    x_range <- as.numeric(x_range)
+    width <- as.numeric(width)
+    boundary <- as.numeric(boundary)
+    shift <- floor((x_range[1] - boundary) / width)
+    origin <- boundary + shift * width
+    
+    # Small correction factor so that we don't get an extra bin when, for
+    # example, origin = 0, max(x) = 20, width = 10.
+    max_x <- x_range[2] + (1 - 1e-08) * width
+    breaks <- seq(origin, max_x, width)
+    
+    bin_breaks(breaks, closed = closed)
+  }
+  
+  bin_breaks_bins <- function(x_range, bins = 30, center = NULL,
+                              boundary = NULL, closed = c('right', 'left')) {
+    stopifnot(length(x_range) == 2)
+    
+    bins <- as.integer(bins)
+    if (bins < 1) {
+      stop('Need at least one bin.', call. = FALSE)
+    } else if (bins == 1) {
+      width <- diff(x_range)
+      boundary <- x_range[1]
+    } else {
+      width <- (x_range[2] - x_range[1]) / (bins - 1)
+    }
+    
+    bin_breaks_width(x_range, width,
+                     boundary = boundary, center = center,
+                     closed = closed
+    )
+  }
+  
+  feature_list <- 
+    dbConnect(
+      MySQL(),
+      user = 'betterlife',
+      password = 'snail132',
+      host = 'betterlife.duckdns.org',
+      port = 1231 ,
+      dbname = 'stock_db') %>% dbGetQuery("select * from feature_list_20210709")
+  
+  #label_format = "%.1e"
+  label_format = "%.3f"
+  
+  data_temp = 
+    test_shap %>% 
+    # 1. 상위 N개 종목만 추출
+    inner_join(
+      test_shap %>% 
+        select(date, stock_cd, pred_mean) %>%
+        unique() %>% 
+        group_by(date) %>% 
+        arrange(desc(pred_mean), .by_group=T) %>% 
+        dplyr::slice(1:50) %>% 
+        ungroup() %>% 
+        select(date, stock_cd),
+      by=c("date", "stock_cd")
+    ) %>% 
+    # 2. 매크로 변수 제외
+    left_join(feature_list %>% filter(category == 'Macro') %>% select(feature, category), by=c("variable"="feature")) %>% 
+    filter(is.na(category)) %>% 
+    select(-category) %>% 
+    # 3. 연도별 주요 변수
+    mutate(yyyy = substr(date, 1, 4)) %>% 
+    select(-stock_cd, -pred_mean, -date) %>% 
+    group_by(yyyy, variable) %>% 
+    mutate(mean_value = mean(mean_value)) %>% 
+    ungroup()
+  
+  data_long =
+    data_temp %>% 
+    inner_join(
+      data_temp %>% 
+        select(yyyy, variable, mean_value) %>% 
+        unique() %>% 
+        group_by(yyyy) %>% 
+        arrange(desc(mean_value), .by_group=T) %>% 
+        dplyr::slice(1:10) %>% 
+        mutate(variable2 = factor(paste0(yyyy,'_',str_pad(n():1, width=2, pad=0, side="left"),'_',variable))) %>% 
+        select(-mean_value),
+      by=c("yyyy", "variable")
+    ) %>% 
+    as.data.table() %>% 
+    arrange(yyyy, desc(mean_value))
+  
+  x_bound <- max(abs(data_long$value))*1.1
+  plot1 <- 
+    ggplot(data = data_long) +
+    coord_flip(ylim = c(-x_bound, x_bound)) +
+    geom_hline(yintercept = 0) + # the y-axis beneath
+    geom_sina(aes(x = variable2, y = value, color = stdfvalue),
+              method = "counts", maxwidth = 0.7, alpha = 0.7) +
+    # print the mean absolute value:
+    geom_text(data = unique(data_long[, c("variable2", "mean_value", "yyyy")]),
+              aes(x = variable2, y=-Inf, label = sprintf(label_format, mean_value)),
+              size = 3, alpha = 0.7,
+              hjust = -0.2,
+              fontface = "bold",
+              check_overlap = TRUE) +
+    scale_color_gradient(low="#0389FA", high="#FF0152",
+                         breaks=c(0,1), labels=c(" Low", "High "),
+                         guide = guide_colorbar(barwidth = 12, barheight = 0.3)) +
+    theme_bw() + 
+    theme(axis.line.y = element_blank(),
+          axis.ticks.y = element_blank(), # remove axis line
+          legend.position="bottom",
+          legend.title=element_text(size=10),
+          legend.text=element_text(size=8),
+          axis.title.x= element_text(size = 10)) + 
+    labs(y = "SHAP value (impact on model output)", x = "", color = "Feature value  ") +
+    facet_wrap(~yyyy, scale="free_y") +
+    scale_x_discrete(labels = function(x) str_replace(x, "\\d{4}_\\d{2}_", ""))
+  return(plot1)
+}
