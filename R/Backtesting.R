774ad8e48fcedafbabedb838b8ab1a77d2871bef
Sang Hyun <40751693+sanghyun614@users.noreply.github.com>
2021-12-13 00:30:30 +0900

    Update Backtesting.R

--- R/Backtesting.R
+++ R/Backtesting.R
@@ -4,7 +4,7 @@
 
 #' @export
 backtest_portfolio =
-  function(test_title="Portfolio Return", ssl_list, topN, pred_col, SN_ratio, FN_ratio, include_issue, upper_bound, lower_bound, min_transaction_amount, safe_haven = NA, weight_list = NA, start_date = '20150106', end_date = '99991231', load_data = 'Y') {
+  function(test_title="Portfolio Return", ssl_list, topN, pred_col, FN_ratio, SN_ratio, include_issue, upper_bound, lower_bound, min_transaction_amount, safe_haven = NA, weight_list = NA, start_date = '20150106', end_date = '99991231', load_data = 'Y') {
 
     transaction_fee_rate = 0.00315
     start_date = str_replace_all(start_date, '-', '')
