17a4acc1b2a8cb177b3c1b6bd3f64c49bd0a5277
Sang Hyun <40751693+sanghyun614@users.noreply.github.com>
2021-12-16 22:38:47 +0900

    Update Distributing Shares.R

--- R/Distributing Shares.R
+++ R/Distributing Shares.R
@@ -3,61 +3,48 @@
 # How many shares
 
 #' @export
-how_many_shares = function(ssl, seed_money, pred_col, topN=30, view_method="long", SN_ratio=0.3, min_transaction_amount=1e8) {
+how_many_shares = function(ssl, seed_money, pred_col, topN=30, SN_ratio=0.3, min_transaction_amount=1e8, view_method="long") {
   Sys.setlocale("LC_CTYPE", "ko_KR.UTF-8")
   if(length(unique(ssl$date)) != 1) {
     stop("There must be only one date in SSL.")
   }
-
-  stock_db_connection <- dbConnect(
-    MySQL(),
-    user = 'betterlife',
-    password = 'snail132',
-    host = 'betterlife.duckdns.org',
-    port = 1231 ,
-    dbname = 'stock_db'
-  )
-  dbSendQuery(stock_db_connection, "SET NAMES utf8;")
-  dbSendQuery(stock_db_connection, "SET CHARACTER SET utf8mb4;")
-  dbSendQuery(stock_db_connection, "SET character_set_connection=utf8mb4;")
-
+  
   inv_date <- str_replace_all(unique(ssl$date),'-','')
   print(paste0("Inv Date: ", inv_date))
-
+  
   # Read Data
-  sector_info = dbGetQuery(stock_db_connection, paste0("select * from stock_market_sector where date = '", inv_date,"';"))
-  Encoding(sector_info$stock_nm) = 'UTF-8'; Encoding(sector_info$sector) = 'UTF-8'
-
-  d_stock_price_temp <-
-    dbGetQuery(stock_db_connection, paste0("select * from stock_adj_price where date = '", inv_date,"';")) %>%
+  d_stock_price_temp <- 
+    dbConnect(MySQL(),
+              user = 'betterlife',
+              password = 'snail132',
+              host = 'betterlife.duckdns.org',
+              port = 1231 ,
+              dbname = 'stock_db') %>% 
+    dbGetQuery(paste0("select * from stock_adj_price where date = '", inv_date,"';")) %>%
     select(stock_cd, date, price = adj_close_price)
-
+  
   ssl_temp =
     ssl %>%
     ta_filtering(min_transaction_amount) %>%
-    left_join(sector_info %>% select(stock_cd, stock_nm, sector, market), by="stock_cd") %>%
-    group_by(sector) %>%
-    arrange(desc(get(pred_col)), .by_group=T) %>%
-    dplyr::slice(1:floor(topN*SN_ratio)) %>%
-    ungroup() %>%
+    sector_neutral(SN_ratio, topN, pred_col) %>% 
     arrange(desc(get(pred_col))) %>%
     dplyr::slice(1:topN) %>%
     left_join(d_stock_price_temp %>% mutate(date=ymd(date)), by=c("stock_cd", "date")) %>%
     mutate(each_stock_cap = seed_money / topN) %>%
     mutate(cnt_temp = floor(each_stock_cap / price)) %>%
     mutate(amt_temp = price * cnt_temp)
-
+  
   surplus = seed_money - sum(ssl_temp$amt_temp)
-
+  
   k=1
   while (unique(surplus) > min(ssl_temp$price)) {
-
+    
     if (k > topN) {
       k = 1
     } else {
       k = k
     }
-
+    
     if(surplus >= ssl_temp$price[k]) {
       ssl_temp$cnt_temp[k] = ssl_temp$cnt_temp[k] + 1
       surplus = surplus - ssl_temp$price[k]
@@ -66,17 +53,17 @@ how_many_shares = function(ssl, seed_money, pred_col, topN=30, view_method="long
       k = k + 1
     }
   }
-
+  
   final_result =
     ssl_temp %>%
     mutate(proportion = paste0(round((cnt_temp*price) / sum(cnt_temp*price) * 100, 2), '%'), amt=price*cnt_temp) %>%
     select(stock_cd, stock_nm, market, all_of(pred_col), cnt=cnt_temp, amt, proportion, last_close_price=price)
-
+  
   print("============================================================================")
   print(paste0("Surplus: ", surplus))
-
+  
   lapply(dbListConnections(dbDriver(drv = "MySQL")), dbDisconnect)
-
+  
   if (view_method == "long") {
     return(final_result)
   } else {
