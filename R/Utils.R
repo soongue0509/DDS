2d4a54637e79564cbc1b4c639ff52080f5d984cf
Sang Hyun <40751693+sanghyun614@users.noreply.github.com>
2022-01-17 22:51:02 +0900

    Update Utils.R

--- R/Utils.R
+++ R/Utils.R
@@ -6,11 +6,10 @@
 ssl_mix <- function(ssl1, ssl2, ssl1_ratio) {
   result <-
     dplyr::inner_join(
-      ssl1 %>% select(date, stock_cd, pred_mean, target_1m_return),
-      ssl2 %>% select(date, stock_cd, pred_mean),
+      ssl1 %>% select(date, stock_cd, ssl1 = pred_mean, target_1m_return),
+      ssl2 %>% select(date, stock_cd, ssl2 = pred_mean),
       by=c("date", "stock_cd")
     ) %>%
-    rename(ssl1=pred_mean.x, ssl2=pred_mean.y) %>%
     mutate(pred_mix = ssl1*ssl1_ratio + ssl2*(1-ssl1_ratio)) %>%
     select(date, stock_cd, ssl1, ssl2, pred_mix, target_1m_return) %>%
     arrange(date) %>%
